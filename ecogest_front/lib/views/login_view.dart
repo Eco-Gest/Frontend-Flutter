f0d39faaf6060113be871bd40d46dd0198906994
LaitAh <lea.clair34@gmail.com>
2023-09-21 19:15:04 +0200

    :tada: [#29 US7-F1] init(login): setting up the login page according to the wireframe + error management of inputs if empty or non-compliant + link redirect to the register page

--- ecogest_front/lib/views/login_view.dart
+++ ecogest_front/lib/views/login_view.dart
@@ -1,5 +1,7 @@
+import 'package:ecogest_front/views/register_view.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginView extends StatelessWidget {
   LoginView({super.key});
@@ -20,9 +22,32 @@ class LoginView extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             // Logo
-            Image.asset('assets/logo/logo-color.png'),
+            Container(
+              alignment: Alignment.topCenter,
+              padding: const EdgeInsets.all(20),
+              child: Image.asset('assets/logo/logo-color.png'),
+            ),
 
-            // Form
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Column(
+                children: [
+                  Text(
+                    'Vous avez déjà un compte ?',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    'Heureux de vous revoir',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ]
+              ),
+            ),
+
             // Input email
             Container(
               alignment: Alignment.topCenter,
@@ -34,10 +59,11 @@ class LoginView extends StatelessWidget {
                   labelText: 'Email',
                   hintText: 'Entrez votre email',
                 ),
-                autovalidateMode: AutovalidateMode.always,
-                validator: (value) => EmailValidator.validate(value!) ? null : "Please enter a valid email",
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) => EmailValidator.validate(value!) ? null : 'Veuillez entrer un email valide',
               ),
             ),
+
             // Input password
             Container(
               alignment: Alignment.topCenter,
@@ -50,17 +76,77 @@ class LoginView extends StatelessWidget {
                   labelText: 'Mot de passe',
                   hintText: 'Entrez votre mot de passe',
                 ),
-                autovalidateMode: AutovalidateMode.always,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
                 validator: (value) => value!.length < 8 ? 'Mot de passe non valide' : null,
               ),
             ),
-            
-              // Forgot password
-              // TODO : Error message
 
-              // Connect button
+            // Forgot password
+            TextButton(
+              onPressed: () {
+                //TODO: redirect to forgot password screen
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Mot de passe oublié ?',
+                ),
+              ),
+            ),
+
+            // TODO: Error message if user is not allow to connect
+
+            // Connect button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(30),
+                alignment: Alignment.topCenter,
+                padding: const EdgeInsets.all(20),
+                foregroundColor: Colors.white, // Color of button text
+                textStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                )
+              ),
+              onPressed: () {
+                debugPrint('Email: ${emailController.text}');
+                debugPrint('Password: ${passwordController.text}');
+                // TODO: Auth user
+              },
+              child: const Text('Se connecter'),
+            ),
 
             // Link s'inscrire
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(40),
+              child: Column(
+                children: [
+                  const Text(
+                    'Vous souhaitez créer un compte ?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      )
+                    ),
+                    onPressed: () {
+                      GoRouter.of(context).goNamed(RegisterView.name);
+                    },
+                    child: const Text(
+                      'S\'inscrire',
+                    ),
+                  ),
+                ]
+              ),
+            ),
+
           ],
         ),
       ),
