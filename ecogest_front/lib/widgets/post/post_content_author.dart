44aaa429cf1e948b44b7ba9b59839efa99183a55
LaitAh <lea.clair34@gmail.com>
2023-11-03 11:14:23 +0100

    :truck: add the possibility of clicking on the autor of a post to access their profile page

--- ecogest_front/lib/widgets/post/post_content_author.dart
+++ ecogest_front/lib/widgets/post/post_content_author.dart
@@ -1,5 +1,7 @@
 import 'package:ecogest_front/models/user_model.dart';
+import 'package:ecogest_front/views/user_view.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 
 class PostContentAuthor extends StatelessWidget {
@@ -16,10 +18,10 @@ class PostContentAuthor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     // Transform date from DB to french date format
     DateTime dateInFormat = DateTime.parse(date.toString());
-    String publicationDate = DateFormat('dd/MM/yyyy', 'fr_FR').format(dateInFormat);
+    String publicationDate =
+        DateFormat('dd/MM/yyyy', 'fr_FR').format(dateInFormat);
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
@@ -28,10 +30,9 @@ class PostContentAuthor extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Text(
-              publicationDate
-            ),
-            if (date != null && (position != null || author!.position != null)) ...[
+            Text(publicationDate),
+            if (date != null &&
+                (position != null || author!.position != null)) ...[
               const Text(' | '),
             ],
             Text(() {
@@ -42,56 +43,62 @@ class PostContentAuthor extends StatelessWidget {
               } else {
                 return '';
               }
-            } ()
-              // 'Rennes, France'
-            ),
+            }()
+                // 'Rennes, France'
+                ),
           ],
         ),
-        Column(
-          children: [
-            Row(
-              children: [
-                if (author!.image != null) ...[
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(author!.image.toString()),
-                  )
-                ] else ...[
-                  const CircleAvatar(
-                    child: Icon(Icons.person),
-                  ),
-                ],
-                const SizedBox(
-                  height: 10,
-                  width: 10,
+        Column(children: [
+          Row(
+            children: [
+              if (author!.image != null) ...[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(author!.image.toString()),
+                )
+              ] else ...[
+                const CircleAvatar(
+                  child: Icon(Icons.person),
                 ),
-                Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(author?.username.toString() ?? 'Username'),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        FilledButton.tonal(
-                          onPressed: () {
-                            // TODO : Afficher les différents badges
-                          },
-                          child: Text(
-                            author?.badgeTitle ?? 'Badge',
-                            style: const TextStyle(
-                              fontSize: 12,
-                            ),
+              ],
+              const SizedBox(
+                height: 10,
+                width: 10,
+              ),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextButton(
+                        child: Text(author?.username.toString() ?? 'Username'),
+                        onPressed: () {
+                          GoRouter.of(context)
+                              .pushNamed(UserView.name, pathParameters: {
+                            'id': author!.id.toString(),
+                          });
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      FilledButton.tonal(
+                        onPressed: () {
+                          // TODO : Afficher les différents badges
+                        },
+                        child: Text(
+                          author?.badgeTitle ?? 'Badge',
+                          style: const TextStyle(
+                            fontSize: 12,
                           ),
                         ),
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ),
-          ] 
-        )
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ])
       ],
     );
   }
