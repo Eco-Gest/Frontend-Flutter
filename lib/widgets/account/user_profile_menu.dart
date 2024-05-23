c743735e9e7fd12df1b32824b0002887ebcc8b62
AugustinSeguin <augustinseguin@gmail.com>
2024-05-23 15:25:43 +0200

    fix bugs follow actions user view

--- lib/widgets/account/user_profile_menu.dart
+++ lib/widgets/account/user_profile_menu.dart
@@ -1,5 +1,7 @@
+import 'package:ecogest_front/models/user_model.dart';
 import 'package:ecogest_front/state_management/authentication/authentication_cubit.dart';
 import 'package:ecogest_front/state_management/users_relation/users_relation_cubit.dart';
+import 'package:ecogest_front/state_management/users_relation/users_relation_state.dart';
 import 'package:flutter/material.dart';
 import 'package:ecogest_front/state_management/user/user_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,116 +9,129 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class UserProfileMenu extends StatelessWidget {
   const UserProfileMenu({
     Key? key,
-    required this.userId,
+    required this.user,
     required this.username,
     required this.isBlocked,
   }) : super(key: key);
 
-  final int? userId;
+  final UserModel user;
   final String username;
   final bool isBlocked;
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton<String>(
-      onSelected: (value) async {
-        if (value == 'report') {
-          final result = await showDialog<String>(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text('Pourquoi signalez-vous ce profil ?'),
-                content: Column(
-                  children: [
-                    ListTile(
-                      title: const Text('Contenu inapproprié et harcèlement'),
-                      onTap: () =>
-                          Navigator.pop(context, 'Contenu inapproprié'),
+    bool isFollowing =user.following?.any((s) =>
+                    s!.followingId ==
+                        context.read<AuthenticationCubit>().state.user!.id! &&
+                    s.status == "approved") ?? false;
+    return BlocBuilder<UsersRelationCubit, UsersRelationState>(
+      builder: (context, state) {
+        return PopupMenuButton<String>(
+          onSelected: (value) async {
+            if (value == 'report') {
+              final result = await showDialog<String>(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Pourquoi signalez-vous ce profil ?'),
+                    content: Column(
+                      children: [
+                        ListTile(
+                          title:
+                              const Text('Contenu inapproprié et harcèlement'),
+                          onTap: () =>
+                              Navigator.pop(context, 'Contenu inapproprié'),
+                        ),
+                        ListTile(
+                          title: const Text('Discours de haine'),
+                          onTap: () =>
+                              Navigator.pop(context, 'Discours de haine'),
+                        ),
+                        ListTile(
+                          title: const Text('Atteinte à la vie privé'),
+                          onTap: () =>
+                              Navigator.pop(context, 'Atteinte à la vie privé'),
+                        ),
+                        ListTile(
+                          title: const Text(
+                              'Suicide ou conduites autodestructrices'),
+                          onTap: () => Navigator.pop(context,
+                              'Suicide ou conduites autodestructrices'),
+                        ),
+                        ListTile(
+                          title: const Text('Nudité ou actes sexuels'),
+                          onTap: () =>
+                              Navigator.pop(context, 'Nudité ou actes sexuels'),
+                        ),
+                        ListTile(
+                          title: const Text('Spam'),
+                          onTap: () => Navigator.pop(context, 'Spam'),
+                        ),
+                        ListTile(
+                          title: const Text('Autre'),
+                          onTap: () => Navigator.pop(context, 'Autre'),
+                        ),
+                      ],
                     ),
-                    ListTile(
-                      title: const Text('Discours de haine'),
-                      onTap: () => Navigator.pop(context, 'Discours de haine'),
-                    ),
-                    ListTile(
-                      title: const Text('Atteinte à la vie privé'),
-                      onTap: () =>
-                          Navigator.pop(context, 'Atteinte à la vie privé'),
-                    ),
-                    ListTile(
-                      title: const Text('Suicide ou conduites autodestructrices'),
-                      onTap: () => Navigator.pop(
-                          context, 'Suicide ou conduites autodestructrices'),
-                    ),
-                    ListTile(
-                      title: const Text('Nudité ou actes sexuels'),
-                      onTap: () =>
-                          Navigator.pop(context, 'Nudité ou actes sexuels'),
-                    ),
-                    ListTile(
-                      title: const Text('Spam'),
-                      onTap: () => Navigator.pop(context, 'Spam'),
-                    ),
-                    ListTile(
-                      title: const Text('Autre'),
-                      onTap: () => Navigator.pop(context, 'Autre'),
-                    ),
-                  ],
-                ),
+                  );
+                },
               );
-            },
-          );
 
-          if (result != null) {
-            await context.read<UserCubit>().submitReport(userId!, result);
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Signalement enregistré. Merci !'),
-              ),
+              if (result != null) {
+                await context.read<UserCubit>().submitReport(user.id!, result);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Signalement enregistré. Merci !'),
+                  ),
+                );
+              }
+            }
+          },
+          itemBuilder: (BuildContext context) {
+            final items = List<PopupMenuItem<String>>.empty(growable: true);
+            if (state is! RemoveFollowerStateSuccess && isFollowing) {
+              final removeFollower = PopupMenuItem<String>(
+                child: const Text('Retirer de mes abonnements'),
+                onTap: () => {
+                  context.read<UsersRelationCubit>().removeFollower(user.id!),
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content:
+                          Text('$username a été retiré de vos abonnements'),
+                    ),
+                  ),
+                },
+              );
+              items.add(removeFollower);
+            }
+            if (context.read<AuthenticationCubit>().state.user?.following?.any(
+                        (s) =>
+                            s!.followingId == user.id! &&
+                            s.status == "blocked") ==
+                    false &&
+                isBlocked == false) {
+              final blockUser = PopupMenuItem<String>(
+                child: const Text('Bloquer'),
+                onTap: () => {
+                  context.read<UsersRelationCubit>().blockUser(user.id!),
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Vous avez bloqué $username'),
+                    ),
+                  ),
+                },
+              );
+              items.add(blockUser);
+            }
+            const report = PopupMenuItem<String>(
+              value: 'report',
+              child: Text('Signaler'),
             );
-          }
-        }
-      },
-      itemBuilder: (BuildContext context) {
-        final items = List<PopupMenuItem<String>>.empty(growable: true);
-        if (context.read<AuthenticationCubit>().state.user?.followers?.any(
-                (s) => s!.followerId == userId && s.status == "approved") ==
-            true) {
-          final removeFollower = PopupMenuItem<String>(
-            child: const Text('Retirer de mes abonnements'),
-            onTap: () => {
-              context.read<UsersRelationCubit>().removeFollower(userId!),
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('$username a été retiré de vos abonnements'),
-                ),
-              ),
-            },
-          );
-          items.add(removeFollower);
-        }
-        if (context.read<AuthenticationCubit>().state.user?.following?.any(
-                (s) => s!.followingId == userId && s.status == "blocked") ==
-            false && isBlocked == false) {
-          final blockUser = PopupMenuItem<String>(
-            child: const Text('Bloquer'),
-            onTap: () => {
-              context.read<UsersRelationCubit>().blockUser(userId!),
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('Vous avez bloqué $username'),
-                ),
-              ),
-            },
-          );
-          items.add(blockUser);
-        }
-        const report = PopupMenuItem<String>(
-          value: 'report',
-          child: Text('Signaler'),
-        );
 
-        items.addAll([report]);
-        return items;
+            items.addAll([report]);
+            return items;
+          },
+        );
       },
     );
   }
